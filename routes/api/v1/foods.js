4b114dd35d7c55097aee001e4e7daa55acb4e601
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-08 16:20:30 -0600

    Add routes for get foods endpoints.

--- routes/api/v1/foods.js
+++ routes/api/v1/foods.js
@@ -3,12 +3,22 @@ var router = express.Router();
 const environment = process.env.NODE_ENV || 'development'
 const configuration = require('../../../knexfile')[environment]
 const database = require('knex')(configuration)
+pry = require('pryjs')
 
 router.get('/', function(req, res, next) {
   database.raw(
-    'SELECT id, name FROM meals'
-  ).then(function(meals) {
-    res.json(meals.rows)
+    'SELECT id, name, calories FROM foods'
+  ).then(function(foods) {
+    res.json(foods.rows)
+  })
+})
+
+router.get('/:id', function(req, res, next) {
+  var id = req.params.id
+  database.raw(
+    'SELECT id, name, calories FROM foods WHERE id=?', [id]
+  ).then(function(food) {
+    res.json(food.rows)
   })
 })
 
