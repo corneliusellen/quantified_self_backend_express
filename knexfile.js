912f72e965c1dab95ef92d20449b0d76f2ff661a
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-10 07:22:14 -0600

    Add database url.

--- knexfile.js
+++ knexfile.js
@@ -25,7 +25,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: 'postgres://yrnzivdzapxvka:0339c9680f10fa7f5f0ce3223b0c80939fa48b0c7dac3e7221899ac22f7d5573@ec2-107-20-249-68.compute-1.amazonaws.com:5432/d30sp9nh0ea0qo',
     migrations: {
       directory: './db/migrations'
     },
