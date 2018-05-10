81a00e2e717c0632c47bfb823ee594b5dd82f3b8
Ellen Cornelius <ecorneli@iwu.edu>
2018-05-09 18:36:29 -0600

    Add foodmeal seeds.

--- db/seeds/test/meals.js
+++ db/seeds/test/meals.js
@@ -1,13 +1,16 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('meals').del()
+  return knex('foodmeals').del()
+    .then(function () {
+      return knex('meals').del()
+    })
     .then(function () {
       return Promise.all([
         knex('meals').insert([
-          {name: 'Breakfast', created_at: new Date},
-          {name: 'Snacks', created_at: new Date},
-          {name: 'Lunch', created_at: new Date},
-          {name: 'Dinner', created_at: new Date}
+          {id: 1, name: 'Breakfast', created_at: new Date},
+          {id: 2, name: 'Snacks', created_at: new Date},
+          {id: 3, name: 'Lunch', created_at: new Date},
+          {id: 4, name: 'Dinner', created_at: new Date}
         ])
       ])
     })
@@ -24,5 +27,16 @@ exports.seed = function(knex, Promise) {
     })
     .then(function(){
     return knex.raw(`ALTER SEQUENCE foods_id_seq RESTART 3`)
+    })
+    .then(function () {
+      return Promise.all([
+        knex('foodmeals').insert([
+          {id: 1, food_id: 1, meal_id: 1},
+          {id: 2, food_id: 2, meal_id: 1}
+        ])
+      ])
+    })
+    .then(function () {
+      return knex.raw(`ALTER SEQUENCE foodmeals_id_seq RESTART 3`)
     })
 }
